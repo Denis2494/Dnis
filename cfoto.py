2eecf77e2189335f834354c61447290ee98e62ef
Denis2494 <75392166+Denis2494@users.noreply.github.com>
2021-03-19 00:26:06 +0700

    Delete cfoto.py

--- cfoto.py
+++
@@ -1,7 +0,0 @@
-from upfoto import *
-from datetime import datetime, timedelta
-import time, random, sys, json, codecs, threading, asyncio, glob, re, string, os, six, ast, pytz, atexit, traceback,requests
-pub = LINE('',appName='')
-a = 'img.jpg'
-try:pub.updateProfilePicture(a)
-except:pub.updateProfilePicture(a)
